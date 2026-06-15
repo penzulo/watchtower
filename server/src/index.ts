a1f9a42d98e9d6089a88b1c452cd1e29aa6b768d
penzulo <bhardesh2005@gmail.com>
2026-06-16 02:09:18 +0530

    test(app): Comprehensive test suite for Elysia
    - add a comprehensive test suite for the Elysia server
    - test against `@elysiajs/eden` as well
    - remove unnecessary comments from server's `tsconfig`

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { v1 } from "@watchtower/server/routes/v1";
 import { Elysia } from "elysia";
 
-const app = new Elysia().use(v1).listen(3000);
+export const app = new Elysia().use(v1).listen(3000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
